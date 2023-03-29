7944fa223bc5fa9dd10b7f68ef8c217dfdbe6c02
rene018 <rene18agosto@gmail.com>
2023-03-29 13:59:04 -0500

    commit

---
+++ Recuperacion/main.js
@@ -0,0 +1,24 @@
+let api = 'https://jsonplaceholder.typicode.com/users'
+// let btn = document.getElementById('formulario')
+
+async function buscar() {
+    const response = await fetch(api)
+    const data = await response.json()
+    localStorage.setItem('users', JSON.stringify(data))
+}
+
+buscar()
+
+let email=document.querySelector('#email')
+let user=document.querySelector('#user')
+btn.addEventListener('submit', function (e) {
+    e.preventDefault()
+    data.forEach(element => {
+        if (email.value==element.email && user.value==element.username) {
+            console.log(element);
+        } else {
+            
+        }
+    });
+    
+})
